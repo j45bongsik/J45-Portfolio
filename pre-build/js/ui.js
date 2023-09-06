210c6caca79af236b07158b0aeb3274ab524da43
j45 <dnrlekzz@naver.com>
2023-09-06 20:21:55 +0900

    애니메이션 추가변경

--- pre-build/js/ui.js
+++ pre-build/js/ui.js
@@ -193,6 +193,9 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
 
+    
+
+
     /* lottie animation */
     const animationDev02 = {
         container: document.querySelector('.lottieItem.developer02'), 
@@ -211,11 +214,166 @@ document.addEventListener("DOMContentLoaded", function () {
         renderer: 'svg', 
         loop: true, 
         autoplay: true, 
-        path: '../images/developer.json',
+        path: '../images/developer03.json',
     };
 
     const animation03 = lottie.loadAnimation(animationDev03);
 
-    
 
+    /* lottie animation */
+    const animationDev04 = {
+        container: document.querySelector('.lottieItem.plane'), 
+        renderer: 'svg', 
+        loop: true, 
+        autoplay: true, 
+        path: '../images/plane.json',
+    };
+
+    const animation04 = lottie.loadAnimation(animationDev04);
+
+
+    /* lottie animation */
+    const animationDev05 = {
+        container: document.querySelector('.lottieItem.cloud01'), 
+        renderer: 'svg', 
+        loop: true, 
+        autoplay: true, 
+        path: '../images/cloud01.json',
+    };
+
+    const animation05 = lottie.loadAnimation(animationDev05);
+
+
+    /* lottie animation */
+    const animationSettings = {
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+    };
+    
+    const cloud02Elements = document.querySelectorAll('.lottieItem.cloud02');
+    
+    cloud02Elements.forEach((cloud02Element, index) => {
+        const animationPath = `../images/cloud02.json`;
+    
+        const animationConfig = {
+            container: cloud02Element,
+            path: animationPath,
+            ...animationSettings,
+        };
+    
+        const animation = lottie.loadAnimation(animationConfig);
+    });
+
+
+    function updateCenteredLottieItemPosition() {
+        const lottieItem = document.getElementById('plane'); // Change 'test' to your actual ID
+        const scrollY = window.scrollY || document.documentElement.scrollTop;
+    
+        newPosition = (scrollY - 900);
+    
+    
+        lottieItem.style.top = newPosition + 'px';
+    }
+    
+    // Add a scroll event listener to update the position on scroll
+    window.addEventListener('scroll', updateCenteredLottieItemPosition);
+    
+    // Initial position setup
+    updateCenteredLottieItemPosition();
+    
+    
+    
+    // Get a reference to your lottieItem
+    const lottieItem = document.getElementById("plane");
+    
+    // Create a GSAP timeline
+    const tl02 = gsap.timeline({
+            scrollTrigger: {
+            trigger: "#section2", // Trigger the animation when #section2 enters the viewport
+            start: "top top",     // Start animation when #section2 reaches the top of the viewport
+            end: "bottom top",    // End animation when #section2 leaves the top of the viewport
+            scrub: true,          // Smoothly scrub the animation as you scroll
+        }
+    });
+    
+    // Define the animation sequence
+    tl02.to(lottieItem, {
+        x: "200%",             // Move to the right edge of the screen
+    })
+    .to(lottieItem, {
+        x: "0%",           // Move to the left edge of the screen
+    })
+    .to(lottieItem, {
+        y: "300%",            // Move down to the bottom of the screen
+    })
+    .to(lottieItem, {
+        x: "500%",            // Move to the right edge of the screen again
+    });
+
+
+    // section2 title textanimation
+    const splitChar = gsap.utils.toArray(".splitChar");
+
+    splitChar.forEach((target) => {
+        let SplitCharClient = new SplitType(target, { type: "chars" });
+
+        //변수 지정하고
+        // let lines = SplitClient.lines;
+        // let words = SplitClient.words;
+        let chars = SplitCharClient.chars;
+
+        //인수 수정하고
+        gsap.from(chars, {
+            yPercent: 100,
+            opacity: 0,
+            duration: 1,
+            ease: "circ.out",
+            stagger: {
+                amount: 1,
+                from: "random"
+            },
+            scrollTrigger: {
+                trigger: target,
+                start: "top 75%",
+                end: "+=400",
+                markers: true,
+            }
+        });
+    });
+
+
+    // section2 desc textanimation
+    const splitLine = gsap.utils.toArray(".splitLine");
+        
+    splitLine.forEach((target) => {
+
+        //변경하고
+        let SplitLineClient = new SplitType(target, { type: "lines" });
+
+        //변수 지정하고
+        let lines = SplitLineClient.lines;
+        // let words = SplitClient.words;
+        // let chars = SplitClient.chars;
+
+        //인수 수정하고
+        gsap.from(lines, {
+            yPercent: 100,
+            opacity: 0,
+            duration: 0.5,
+            ease: "circ.out",
+            stagger: {
+                amount: 1,
+                from: "random"
+            },
+            scrollTrigger: {
+                trigger: target,
+                start: "top 90%",
+                end: "+=400",
+                markers: true,
+            }
+        });
+    });
+
+    
 });
